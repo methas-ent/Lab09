a48cfe424b44ad7302802df56c42b156329cd936
Methas <methas_n@cmu.ac.th>
2025-12-20 18:36:16 +0700

    "lab9_2.cpp completed"

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -2,13 +2,27 @@
 using namespace std;
 
 //Write the function printO() here
+void printO(int m , int n){
+    
+	if(m<=0 || n<=0){
+		cout<<"Invalid input";
+	}
+
+    for(int i = 0 ; i < m ; i++){
+		for(int j = 0 ; j < n ; j++){
+        	cout<<"O";
+		}
+		cout<<endl;
+    }
+    
+}
 
 int main(){
 	
 	printO(2,2);
 	cout << "\n";
 	
-	printO(3,5);
+	printO(3,5);// 3 บรรทัด 5 ตัว
 	cout << "\n";	
 	
 	printO(5,3);
